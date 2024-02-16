35ddbc77e2128cbc437bb1328fac91a222db54d7
FelixAlvo <felix@notima.se>
2024-02-16 09:54:25 +0100

    update windows file

--- update.py
+++ update.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import requests
+from time import sleep
 
 def main():
     if len(sys.argv) < 3:
@@ -16,12 +17,44 @@ def main():
         print("Firmware file not found:", firmware_file)
         sys.exit(1)
 
+    def change_protocol(ip_address, protocol):
+        payload = {"config": {"meter": {"protocol": protocol}}}
+
+        change_protocol_response = requests.post("http://" + ip_address + "/rpc/Config.set", json = payload)
+        
+        if change_protocol_response.status_code != 200:
+            raise Exception("Could not change protocol for", ip_address, ". Status: " + str(change_protocol_response.status_code))
+        save_protocol = requests.post("http://" + ip_address + "/rpc/Config.save")
+        
+        if change_protocol_response.status_code != 200:
+            raise Exception("Could not save config for", ip_address)
+            
+        reboot_response_1 = requests.get("http://" + ip_address + "/rpc/sys.reboot")
+        
+        if reboot_response_1.status_code != 200:
+            raise Exception("Could not reboot", ip_address, ". status:", reboot_response_1.status_code)
+        else:
+            sleep(5)
+
     def update_firmware(ip_address):
+        try:
+            change_protocol(ip_address, 201)
+        except Exception as error:
+            print("failed to change protocol", error)
+            return
+            
         response = requests.post("http://{}/update".format(ip_address), files={"file": open(firmware_file, "rb")})
+        
         if response.status_code == 200:
-            print("Firmware update successful for", ip_address)
+            try:
+                change_protocol(ip_address, 0)
+                print("Firmware update successful for", ip_address)
+            except Exception as error:
+                print("failed to restore protocol", error)
         else:
             print("Firmware update failed for", ip_address)
+            return
+        
 
     if os.path.isfile(ip_address_or_list_file):
         with open(ip_address_or_list_file, "r") as f:
